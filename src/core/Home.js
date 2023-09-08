e9640fb95b894cd8dbc28acf66b4af8401ef29fd
MD NADEEM SARWAR <mdnadeem.mn289@gmail.com>
2023-09-08 21:59:15 +0530

    added some lines for testing purpose

--- src/core/Home.js
+++ src/core/Home.js
@@ -4,7 +4,8 @@ import "../styles.css"
 export default function Home() {
   return (
     <div>
-      <h1 className='text-white'>Hello frontEnd</h1>
+      <h1 className='text-white'>Hello front End</h1>
+      <p className='text-white text-center'>About the App</p>
     </div>
   )
 }
