9f546103ad84528c7527cd4bb95bb88b56081926
MD NADEEM SARWAR <mdnadeem.mn289@gmail.com>
2023-09-13 01:05:00 +0530

    Added private routes and conditional tab of navbar

--- src/core/Navbar.js
+++ src/core/Navbar.js
@@ -1,66 +1,89 @@
-import React from 'react'
-import {Link,withRouter} from 'react-router-dom'
-import { signout, isAuthenticate } from '../auth/helper'
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticate } from "../auth/helper";
 
+const currentTab = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#2ecc72" };
+  } else {
+    return { color: "#FFFFFF" };
+  }
+};
 
-
-const currentTab = (history,path) =>{
-    if(history.location.pathname === path){
-        return {color : "#2ecc72"}
-    }else{
-        return {color : "#FFFFFF"}
-    }
-}
-
-const Navbar = ({ history}) => {
-
+const Navbar = ({ history }) => {
   return (
     <div>
-       <ul className="nav nav-tabs bg-dark">
-          <li className="nav-item">
-            <Link style={currentTab(history,"/")} className="nav-link" to="/">
-                Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link style={currentTab(history,"/cart")} className="nav-link" to="/cart">
-                Cart
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link style={currentTab(history,"/user/dasboard")} className="nav-link" to="/user/dashboard">
-                Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link style={currentTab(history,"/admin/dashboard")} className="nav-link" to="/admin/dashboard">
-                Admin Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link style={currentTab(history,"/signup")} className="nav-link" to="/signup">
-                Sing Up
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link style={currentTab(history,"/signin")} className="nav-link" to="/signin">
-                Sing In
-            </Link>
-          </li>
-          {isAuthenticate() && (
+      <ul className="nav nav-tabs bg-dark">
+        <li className="nav-item">
+          <Link style={currentTab(history, "/")} className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            style={currentTab(history, "/cart")}
+            className="nav-link"
+            to="/cart"
+          >
+            Cart
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            style={currentTab(history, "/user/dasboard")}
+            className="nav-link"
+            to="/user/dashboard"
+          >
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            style={currentTab(history, "/admin/dashboard")}
+            className="nav-link"
+            to="/admin/dashboard"
+          >
+            Admin Dashboard
+          </Link>
+        </li>
+        {!isAuthenticate() && (
+          <Fragment>
             <li className="nav-item">
-             <span className="nav-link text-warning"
-              onClick={() =>{
-                signout(() =>{
-                  history.push("/")
-                })
+              <Link
+                style={currentTab(history, "/signup")}
+                className="nav-link"
+                to="/signup"
+              >
+                Sing Up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                style={currentTab(history, "/signin")}
+                className="nav-link"
+                to="/signin"
+              >
+                Sing In
+              </Link>
+            </li>
+          </Fragment>
+        )}
+        {isAuthenticate() && (
+          <li className="nav-item">
+            <span
+              className="nav-link text-warning"
+              onClick={() => {
+                signout(() => {
+                  history.push("/");
+                });
               }}
-             >Signout</span>
+            >
+              Signout
+            </span>
           </li>
-          )}
-       </ul>
-
+        )}
+      </ul>
     </div>
-  )
-}
+  );
+};
 export default withRouter(Navbar);
